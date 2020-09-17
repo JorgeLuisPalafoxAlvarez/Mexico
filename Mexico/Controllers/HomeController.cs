a547ebd2d652c0abeb4aa62f7f38d9e99d517543
JorgeLuisPalafoxAlvarez <171g0241@rcarbonifera.tecnm.mx>
2020-09-17 16:27:22 -0500

    Version 1

---
+++ Mexico/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mexico.Models;
+
+namespace Mexico.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+
+            return View(estados);
+        }
+    }
+}
